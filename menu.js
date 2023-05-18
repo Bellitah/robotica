2888cd931c84a9674a87ca053bad4749cf85eef3
Isabella Lopes Pereira <105372986+Bellitah@users.noreply.github.com>
2023-05-18 20:22:56 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,22 @@
+
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+item.addEventListener('click', selectLink)
+)
+
+// expandir o menu
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
