3a3053699d88f20e645d7130a8e4a095d03847de
Isabella Lopes Pereira <105372986+Bellitah@users.noreply.github.com>
2023-07-22 22:32:42 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,15 +1,27 @@
+const button = document.querySelector("button")
+const modal = document.querySelector("dialog")
+const buttonClose = document.querySelector("dialog button")
+
+button.onclick = function () {
+    modal.showModal()
+}
+
+buttonClose.onclick = function () {
+modal.close()
+}
+
 
 var menuItem = document.querySelectorAll('.item-menu')
 
-function selectLink(){
-    menuItem.forEach((item)=>
+function selectLink() {
+    menuItem.forEach((item) =>
         item.classList.remove('ativo')
     )
     this.classList.add('ativo')
 }
 
-menuItem.forEach((item)=>
-item.addEventListener('click', selectLink)
+menuItem.forEach((item) =>
+    item.addEventListener('click', selectLink)
 )
 
 // expandir o menu
@@ -17,6 +29,6 @@ item.addEventListener('click', selectLink)
 var btnExp = document.querySelector('#btn-exp')
 var menuSide = document.querySelector('.menu-lateral')
 
-btnExp.addEventListener('click', function(){
+btnExp.addEventListener('click', function () {
     menuSide.classList.toggle('expandir')
 })
